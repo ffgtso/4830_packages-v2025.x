d037b4c422a9b4c203cea0035fb468861ee9e496
wusel <wusel+src@uu.org>
2026-02-01 20:07:35 +0100

    Fixes

--- 4830-erx-finalize/files/lib/gluon/erx-upgrade/download-config.sh
+++ 4830-erx-finalize/files/lib/gluon/erx-upgrade/download-config.sh
@@ -6,6 +6,8 @@ FILE="/tmp/${ID}.tgz"
 wget -O ${FILE} "https://erx.4830.org/index.php/download?id=${ID}"
 if [ $? -eq 0 ]; then
   sysupgrade --restore-backup ${FILE} ||:
+  uci set system.@system[0].compat_version='2.0'
+  uci commit system
   gluon-reconfigure ||:
   sync
   sleep 2
